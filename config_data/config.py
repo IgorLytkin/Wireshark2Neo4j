91d95ced906b10d923f8bdd9800d7bfa070fa1f1
IgorLytkin <igor.lytkin.2020@ya.ru>
2024-01-23 21:12:45 +0700

    Добавил загрузку параметров из файла .env

--- config_data/config.py
+++ config_data/config.py
@@ -1,21 +1,22 @@
 from dataclasses import dataclass
 from environs import Env
 
-
 @dataclass
 class Neo4jCredentials:
     user: str       # Логин в Neo4j
     password: str   # Пароль в Neo4j
 
-
 @dataclass
 class Config:
-    neo4j_creds: Neo4jCredentials
-
+    neo4j_uri: str                  # URI сервера Neo4j (например, bolt://localhost:7687)
+    neo4j_creds: Neo4jCredentials   # логин и пароль на сервере Neo4j
+    file_pcapng: str                # путь к файлу с трафиком в формате .pcapng
 
 # Создаем функцию, которая будет читать файл .env и возвращать
-# экземпляр класса Config с заполненными полями token и admin_ids
+# экземпляр класса Config с заполненными полями neo4j_uri, neo4j_creds (user,password), file_pcapng
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
-    return Config(neo4j_creds= Neo4jCredentials(user=env('NEO4J_USER'), password=env('NEO4J_PASSWORD')))
+    return Config(neo4j_uri=env('NEO4J_URI'),
+                  neo4j_creds= Neo4jCredentials(user=env('NEO4J_USER'),password=env('NEO4J_PASSWORD')),
+                  file_pcapng=env('FILE_PCAPNG'))
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ cap: FileCapture
 pkt: Packet
 layer: XmlLayer
 
+
 class Neo4jWorld:
 
     def __init__(self, uri, user, password):
@@ -17,9 +18,9 @@ class Neo4jWorld:
     def close(self):
         self.driver.close()
 
-    def print_greeting(self, message):
+    def start_node(self, message):
         with self.driver.session() as session:
-            greeting = session.execute_write(self._create_and_return_greeting, message)
+            greeting = session.execute_write(self._create_and_return_start_node, message)
             print(greeting)
 
     def create_packet(self, message):           # Создать узел Пакет
@@ -28,8 +29,8 @@ class Neo4jWorld:
             print(packet)
 
     @staticmethod
-    def _create_and_return_greeting(tx, message):
-        result = tx.run("CREATE (a:Greeting) "
+    def _create_and_return_start_node(tx, message):
+        result = tx.run("CREATE (a:FilePcapng) "
                         "SET a.message = $message "
                         "RETURN a.message + ', from node ' + id(a)", message=message)
         return result.single()[0]
@@ -41,18 +42,18 @@ class Neo4jWorld:
                         "RETURN p.message + ', from node ' + id(p)", message=message)
         return result.single()[0]
 
+
 if __name__ == "__main__":
     config: Config = load_config()
-    packetgraph = Neo4jWorld("bolt://localhost:7687", config.neo4j_creds.user, config.neo4j_creds.password)
-    packetgraph.print_greeting("hello, world")
-    packetgraph.close()
+    packetgraph = Neo4jWorld(config.neo4j_uri, config.neo4j_creds.user, config.neo4j_creds.password)
+    packetgraph.start_node(config.file_pcapng) # Создаём первый узел графа = имя входного файла
 
-    cap = pyshark.FileCapture(r'C:\Users\igorl\OneDrive\Документы\KPM-2.pcapng')
+    cap = pyshark.FileCapture(config.file_pcapng)
     for pkt in cap:  # для каждого пакета из файла захвата трафика
         print(pkt)
         print(pkt.frame_info)
         print(pkt.layers)
-        packetgraph._create_packet(pkt)
+        # ToDo: packetgraph.create_packet(pkt)
 
         i: int = 1
         for layer in pkt.layers:  # для каждого слоя пакета
@@ -60,3 +61,6 @@ if __name__ == "__main__":
             i += 1
 
         # ToDo: Создаем узлы в Neo4j: Пакет, Слой, Источник, Приёмник
+
+    # Закрываем соединение с сервером Neo4j
+    packetgraph.close()
