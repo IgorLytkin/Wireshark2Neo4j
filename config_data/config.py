2ff13f5afc3969491c9d9dadb8e0d0f02c1d7fc8
IgorLytkin <igor.lytkin.2020@ya.ru>
2024-03-03 21:21:15 +0700

    Исправил config.py

--- config_data/config.py
+++ config_data/config.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 from environs import Env
 
+
 @dataclass
 class Neo4jCredentials:
     user: str       # Логин в Neo4j
     password: str   # Пароль в Neo4j
+
 
 @dataclass
 class Config:
@@ -14,9 +16,11 @@ class Config:
 
 # Создаем функцию, которая будет читать файл .env и возвращать
 # экземпляр класса Config с заполненными полями neo4j_uri, neo4j_creds (user,password), file_pcapng
+
+
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
     return Config(neo4j_uri=env('NEO4J_URI'),
-                  neo4j_creds= Neo4jCredentials(user=env('NEO4J_USER'),password=env('NEO4J_PASSWORD')),
+                  neo4j_creds=Neo4jCredentials(user=env('NEO4J_USER'), password=env('NEO4J_PASSWORD')),
                   file_pcapng=env('FILE_PCAPNG'))
